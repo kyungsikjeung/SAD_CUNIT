01d3e7256d534c32ddffcf5da2380ab632f8c038
kyungsik jeung <datamining7830@gmail.com>
2025-03-11 17:35:15 +0900

    block erase , sector erase 로직 추가

--- usbtest/test9/main.c
+++ usbtest/test9/main.c
@@ -19,10 +19,13 @@ typedef unsigned long DWORD;
 
 #define BLOCK_SIZE (10U)
 #define SECTOR_SIZE (5)
+#define PAGE_SIZE (3)
 
 
 #define SETOR_ERASE_OP 0x01
 #define BLOCK_ERASE_OP 0x02
+#define PAGE_WRITE_OP 0x03
+
 
 
 typedef enum {
@@ -39,25 +42,18 @@ void EraseByAddress(ERASE_TYPE type, uint32_t adress){
     tData[2] = (adress >> 8) & 0xFF;
     tData[3] = (adress) & 0xFF; // LSB of the memory Address
     // Do something .... GMSL 
-    // print tData
+    // 실제로 지울 로직에 대해서 작성 
     printf("Erase Type: %d, Address: %d\n", tData[0], (tData[1] << 16) | (tData[2] << 8) | tData[3]);
 }
 
 
-
+// Erase Function
 void Erase(unsigned long fileSize){
     DWORD chunkBlockNum  = fileSize / BLOCK_SIZE; 
-    assert(chunkBlockNum ==4);
-    bool isRemainder = (fileSize % SECTOR_SIZE) != 0;
-    assert(isRemainder == false);
-    DWORD chunkSectorNum = (fileSize - (BLOCK_SIZE * chunkBlockNum))  / SECTOR_SIZE;
-    
-    if(isRemainder){
-        chunkSectorNum += 1;
-    }
-
+    DWORD chunkSectorNum = (fileSize - (BLOCK_SIZE * chunkBlockNum))  / SECTOR_SIZE + 1;
+    assert(chunkBlockNum == 2);
     uint32_t startAddr = 0;
-    assert(chunkBlockNum == 4);
+    
     for(int i=0; i<chunkBlockNum; i++){
         EraseByAddress(BLOCK, startAddr);
         startAddr += BLOCK_SIZE;
@@ -68,9 +64,53 @@ void Erase(unsigned long fileSize){
     }
 }
 
+
+void setDummyData(uint8_t* data, int size){
+    for(int i=0; i<size; i++){
+        data[i] = i;
+    }
+}
+
+
+
+
+
+
+void SPI_PAGE_WRTIE(uint32_t addr, uint8_t size, uint8_t* data){
+    uint8_t tData[PAGE_SIZE+4]; 
+    tData[0] = (uint8_t)PAGE_WRITE_OP; // Page Write OP
+    tData[1] = (addr >> 16) & 0xFF;  // MSB of the memory Address
+    tData[2] = (addr >> 8) & 0xFF;
+    tData[3] = (addr) & 0xFF; // LSB of the memory Address
+    memcpy(&tData[4], data, PAGE_SIZE);
+    for(int i=0; i< PAGE_SIZE+4; i++){
+        printf("0x%d\t ", tData[i]);
+    }
+    printf("\r\npage Write Done\r\n");
+} 
+
+
+// Host msd. c 
 int main(){
-    unsigned long file_size = 40;
-    Erase(file_size);
+    unsigned long file_size = 46;
+    uint8_t fileData[file_size];
+    memset(fileData, 0, sizeof(fileData));
+    Erase(file_size); // Block & Sector Erase Test
+    setDummyData(fileData, file_size); // Create Dummy Data for SPI ....
+    DWORD fileChunkNum = (file_size/ PAGE_SIZE)+1;
+    unsigned long  byteRemaining = file_size;
+    uint32_t startAddr = 0;
+    for(DWORD i=0; i<fileChunkNum; i++){
+        if(byteRemaining < PAGE_SIZE){
+            SPI_PAGE_WRTIE(startAddr, byteRemaining, fileData);
+            break;
+        }else{
+            SPI_PAGE_WRTIE(startAddr,PAGE_SIZE, fileData);
+        }
+        startAddr += PAGE_SIZE;
+        byteRemaining -= PAGE_SIZE;
+    }
+
     return 0;
 }
 
