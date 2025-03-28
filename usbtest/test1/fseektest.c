81f4d936693eaaa43cad0a0455789e9d161bca02
kyungsik jeung <datamining7830@gmail.com>
2025-03-28 16:46:49 +0900

    USBtest Bin파일 47 섹터까지 올리는 더미 데이터 수정

--- usbtest/test1/fseektest.c
+++ usbtest/test1/fseektest.c
@@ -4,7 +4,7 @@
   * Purpose: 1~512 숫자로 bin 파일 생성
   * Description: 1~512 1바이트 단위 생성 -> ex) 15 -> 00001111
   */
-
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <direct.h> // _getcwd 함수 사용을 위한 헤더 파일
@@ -22,23 +22,23 @@ unsigned int toBigEndian(unsigned int littleEndian) {
 
 int main(int argc, char *argv[]) {
 
-    printf("인수 개수: %d\n", argc);
-    for (int i = 0; i < argc; i++) {
-        printf("인수[%d]: %s\n", i, argv[i]);
-    }
+    // printf("인수 개수: %d\n", argc);
+    // for (int i = 0; i < argc; i++) {
+    //     printf("인수[%d]: %s\n", i, argv[i]);
+    // }
 
-    char cwd[1024];
-    if (_getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("현재 작업 디렉토리: %s\n", cwd);
-    } else {
-        perror("현재 작업 디렉토리 확인 실패");
-    }
+    // char cwd[1024];
+    // if (_getcwd(cwd, sizeof(cwd)) != NULL) {
+    //     printf("현재 작업 디렉토리: %s\n", cwd);
+    // } else {
+    //     perror("현재 작업 디렉토리 확인 실패");
+    // }
     FILE *pFile;
     int sectorSize = 1024;
     int sectorNum = 49;
-    int numbers[50176];
-    for (int i = 0; i < 50176; i++) {
-        numbers[i] = (i + 1)%256;
+    uint8_t numbers[49152];
+    for (size_t i = 0; i < 49152; i++) {
+        numbers[i] = (i)%256;
     }
     size_t numElements = sizeof(numbers) / sizeof(numbers[0]);
     printf("numElements: %zu\n", numElements);
@@ -52,8 +52,8 @@ int main(int argc, char *argv[]) {
 
     // 배열을 빅 엔디안 형식으로 변환하여 바이너리 파일에 쓰기
     for (size_t i = 0; i < numElements; i++) {
-        unsigned int bigEndianNumber = toBigEndian(numbers[i]);
-        size_t written = fwrite(&bigEndianNumber, sizeof(int), 1, pFile);
+       // unsigned int bigEndianNumber = toBigEndian(numbers[i]);
+        size_t written = fwrite(&numbers[i], sizeof(uint8_t), 1, pFile);
         if (written != 1) {
             perror("파일 쓰기 실패");
             fclose(pFile);
